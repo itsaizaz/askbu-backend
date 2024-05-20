88aba8df1b9a36b0f5f44255afe6ed17e7936c04
Muhammad Aizaz <muhammadaizaz650@outlook.com>
2024-05-21 02:56:57 +0500

    Integrated routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const indexRouter = require('../routes/index');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Setup Middleware
+app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', indexRouter);
 
 module.exports = app;
