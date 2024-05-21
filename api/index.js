34bada9b22edbc8284e7a34c5a65b45dd98cd3ae
Muhammad Aizaz <muhammadaizaz650@outlook.com>
2024-05-21 11:36:31 +0500

    Fix route configuration for /talk endpoint

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('../routes/index');
 const bodyParser = require('body-parser');
+var app = express();
 
-const indexRouter = require('../routes/index');
-
-const app = express();
-
-// Middleware setup
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(bodyParser.json());
-app.use(cors({ origin: '*' }));
+var corsOptions = {
+  origin: '*'
+};
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,9 +26,19 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Routes setup
 app.use('/', indexRouter);
 
-// Fallback for undefined routes
-app.use((req, res, next) => {
-  res.status(404).send('Sorry, can\'t find that!');
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
+
+
